5502d0542c0d509f81f496ced3418f93aa9dcfce
kalec267 <kalec267@gmail.com>
2026-07-09 15:13:54 +0900

    JDK21 first commit

---
+++ src/main/java/com/sweetscoop/SweetScoopBackApplication.java
@@ -0,0 +1,13 @@
+package com.sweetscoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetScoopBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetScoopBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sweetscoop/SweetScoopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweetscoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetScoopBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
